f447f8d68659a24c767a6d65ff307144eeeb8d15
viralisarvadhi <virali.sarvadhi@gmail.com>
2026-01-17 17:06:54 +0530

    updated

--- models/User.js
+++ models/User.js
@@ -6,13 +6,20 @@ const User = sequelize.define("User", {
     name: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true, // adding constarint
+        validate: {
+            isAlpha: true,  // validator that chceck name contain only letters a–z
+            len: [3, 20],   // validator that check the lenth between 3 to 20
+
+
+        },
     },
+
     lastname: {
         type: DataTypes.STRING,
         //allowNull: false,
     },
-}
-);
+});
 console.log(User === sequelize.models.User); // true
 module.exports = User;
 
